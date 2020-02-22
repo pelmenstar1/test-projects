b2a7879a798cbe9a0659a316c34bff6202fa98fb
pelmenstar1 <55542136+pelmenstar1@users.noreply.github.com>
2020-02-22 12:26:25 +0200

    Add project files.

---
+++ AvaloniaBindingTest/ViewModels/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace AvaloniaBindingTest.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+        private string _str = "#0000";
+        public string String
+        {
+            get => _str;
+            set
+            {
+                if (_str != null && _str.Length > 0 && _str[0] == '#' && _str.Length <= 5)
+                {
+                    _str = value;
+                }
+
+                this.RaisePropertyChanged(nameof(String));
+            }
+        }
+    }
+}
